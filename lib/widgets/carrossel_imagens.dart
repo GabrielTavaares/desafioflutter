176bdfad9b75879f6bd69f390f47e9dabe8b9780
Gabriel Tavares <gabrieltavares72@gmail.com>
2022-04-08 22:17:45 -0300

    Ajustes de navegação

--- lib/widgets/carrossel_imagens.dart
+++ lib/widgets/carrossel_imagens.dart
@@ -1,3 +1,4 @@
+import 'package:desafioflutter/main.dart';
 import 'package:desafioflutter/model/produtos.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -16,117 +17,129 @@ class CarrosselImagemWidget extends StatelessWidget {
       items: produtos
           .map(
             (item) => Container(
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(20)),
-                child: Stack(
-                  children: <Widget>[
-                    Image.network(
-                      item.imagem,
-                      loadingBuilder: (BuildContext context, Widget child,
-                          ImageChunkEvent? loadingProgress) {
-                        if (loadingProgress == null) {
-                          return child;
-                        }
-                        return Center(
-                          child: CircularProgressIndicator(
-                            value: loadingProgress.expectedTotalBytes != null
-                                ? loadingProgress.cumulativeBytesLoaded /
-                                    loadingProgress.expectedTotalBytes!
-                                : null,
-                          ),
-                        );
-                      },
-                      fit: BoxFit.cover,
-                      width: double.maxFinite,
+              child: InkWell(
+                onTap: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetalheProduto(
+                        produto: item,
+                      ),
                     ),
-                    Positioned(
-                      bottom: 30,
-                      left: 30,
-                      right: 0,
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: SizedBox(
-                          width: double.maxFinite,
-                          child: Container(
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 10),
-                                  child: Align(
-                                    alignment: Alignment.topLeft,
-                                    child: Container(
-                                      decoration: const BoxDecoration(
-                                        color: Colors.blue,
-                                        borderRadius: BorderRadius.all(
-                                          Radius.circular(5),
-                                        ),
-                                      ),
-                                      child: const Padding(
-                                        padding: EdgeInsets.all(2),
-                                        child: Text(
-                                          'NOVO',
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 12,
-                                              fontWeight: FontWeight.w700),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  decoration: const BoxDecoration(
-                                    gradient: LinearGradient(
-                                      // begin: Alignment.centerLeft,
-                                      // end: Alignment.centerRight,
-                                      colors: [
-                                        Color.fromARGB(255, 221, 221, 221),
-                                        Colors.transparent,
-                                        Colors.transparent,
-                                      ],
-                                    ),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(3),
-                                    child: Column(
-                                      children: [
-                                        SizedBox(
-                                          width: double.maxFinite,
-                                          child: Text(
-                                            item.nome,
-                                            style: const TextStyle(
-                                              color: Color(0xff333333),
-                                              fontSize: 22,
-                                              fontWeight: FontWeight.bold,
-                                            ),
+                  ),
+                },
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.network(
+                        item.imagem,
+                        loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent? loadingProgress) {
+                          if (loadingProgress == null) {
+                            return child;
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes != null
+                                  ? loadingProgress.cumulativeBytesLoaded /
+                                      loadingProgress.expectedTotalBytes!
+                                  : null,
+                            ),
+                          );
+                        },
+                        fit: BoxFit.cover,
+                        width: double.maxFinite,
+                      ),
+                      Positioned(
+                        bottom: 30,
+                        left: 30,
+                        right: 0,
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: SizedBox(
+                            width: double.maxFinite,
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 10),
+                                    child: Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Container(
+                                        decoration: const BoxDecoration(
+                                          color: Colors.blue,
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(5),
                                           ),
                                         ),
-                                        Padding(
-                                          padding:
-                                              const EdgeInsets.only(top: 5),
-                                          child: SizedBox(
+                                        child: const Padding(
+                                          padding: EdgeInsets.all(2),
+                                          child: Text(
+                                            'NOVO',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w700),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: const BoxDecoration(
+                                      gradient: LinearGradient(
+                                        // begin: Alignment.centerLeft,
+                                        // end: Alignment.centerRight,
+                                        colors: [
+                                          Color.fromARGB(255, 221, 221, 221),
+                                          Colors.transparent,
+                                          Colors.transparent,
+                                        ],
+                                      ),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(3),
+                                      child: Column(
+                                        children: [
+                                          SizedBox(
                                             width: double.maxFinite,
                                             child: Text(
-                                              "R\$ ${currency.format(item.preco)}",
+                                              item.nome,
                                               style: const TextStyle(
                                                 color: Color(0xff333333),
-                                                fontSize: 12,
+                                                fontSize: 22,
                                                 fontWeight: FontWeight.bold,
                                               ),
                                             ),
                                           ),
-                                        ),
-                                      ],
+                                          Padding(
+                                            padding:
+                                                const EdgeInsets.only(top: 5),
+                                            child: SizedBox(
+                                              width: double.maxFinite,
+                                              child: Text(
+                                                "R\$ ${currency.format(item.preco)}",
+                                                style: const TextStyle(
+                                                  color: Color(0xff333333),
+                                                  fontSize: 12,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/widgets/lista_mais_populares.dart
+++ lib/widgets/lista_mais_populares.dart
@@ -1,3 +1,4 @@
+import 'package:desafioflutter/main.dart';
 import 'package:flutter/material.dart';
 import 'package:desafioflutter/model/produtos.dart';
 import 'package:intl/intl.dart';
@@ -45,127 +46,139 @@ class ListaMaisPopularesWidget extends StatelessWidget {
               ],
             ),
           ),
-          ..._buildListaCard(),
+          ..._buildListaCard(context),
         ],
       ),
     );
   }
 
-  List<Widget> _buildListaCard() {
+  List<Widget> _buildListaCard(context) {
     var retorno = produtos
         .map(
-          (item) => Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: Container(
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 80,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: Image(
-                        width: 85,
-                        image: NetworkImage(
-                          item.imagem.toString(),
+          (item) => InkWell(
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetalheProduto(
+                    produto: item,
+                  ),
+                ),
+              ),
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container(
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 80,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Image(
+                          width: 85,
+                          image: NetworkImage(
+                            item.imagem.toString(),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: SizedBox(
-                      width: 300,
-                      child: Column(
-                        children: [
-                          Container(
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: CircleAvatar(
-                                      radius: 15,
-                                      backgroundColor: Colors.transparent,
-                                      backgroundImage: NetworkImage(
-                                        item.imagemMarca,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                const Expanded(
-                                  child: Align(
-                                    alignment: Alignment.centerRight,
-                                    child: Icon(
-                                      Icons.favorite_rounded,
-                                      color: Colors.black,
-                                      size: 15,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            child: SizedBox(
-                              width: double.maxFinite,
-                              child: Column(
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: SizedBox(
+                        width: 300,
+                        child: Column(
+                          children: [
+                            Container(
+                              child: Row(
                                 children: [
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      item.nome,
-                                      style: const TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.w700,
+                                  Expanded(
+                                    child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: CircleAvatar(
+                                        radius: 15,
+                                        backgroundColor: Colors.transparent,
+                                        backgroundImage: NetworkImage(
+                                          item.imagemMarca,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                  Container(
-                                    child: Row(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        Container(
-                                          child: const Icon(
-                                            Icons.star_rounded,
-                                            size: 12,
-                                            color: const Color(0xFFFFC107),
-                                          ),
-                                        ),
-                                        Container(
-                                          child: Container(
-                                            child: Text(
-                                              "${item.avaliacao}",
-                                              style: const TextStyle(
-                                                fontSize: 8,
-                                                color: Colors.black,
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      "R\$ ${currency.format(item.preco)}",
-                                      style: const TextStyle(
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.w700,
+                                  const Expanded(
+                                    child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Icon(
+                                        Icons.favorite_rounded,
+                                        color: Colors.black,
+                                        size: 15,
                                       ),
                                     ),
                                   ),
                                 ],
                               ),
                             ),
-                          )
-                        ],
+                            Container(
+                              child: SizedBox(
+                                width: double.maxFinite,
+                                child: Column(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        item.nome,
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      child: Row(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          Container(
+                                            child: const Icon(
+                                              Icons.star_rounded,
+                                              size: 12,
+                                              color: const Color(0xFFFFC107),
+                                            ),
+                                          ),
+                                          Container(
+                                            child: Container(
+                                              child: Text(
+                                                "${item.avaliacao}",
+                                                style: const TextStyle(
+                                                  fontSize: 8,
+                                                  color: Colors.black,
+                                                  fontWeight: FontWeight.w700,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        "R\$ ${currency.format(item.preco)}",
+                                        style: const TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
